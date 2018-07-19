2d58b3be904f48f06a00b04f16bb69ae893d10c6
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-19 10:15:39 +0200

    Define T shape and draw it on the board

--- js/tetris.js
+++ js/tetris.js
@@ -1,6 +1,31 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
+// set up board
+
+context.scale(20, 20);
+
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
 
+// define shapes
+
+// T shape
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+];
+
+function drawShape(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        });
+    });
+}
+
+drawShape(matrix, {x: 5, y: 5});
