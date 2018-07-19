ac681994228082d9ba9c593a6ef43ca273e9d9cd
Robert Klim <kontakt.robert.klim@gmail.com>
2018-07-19 10:47:58 +0200

    Add controlls

--- js/tetris.js
+++ js/tetris.js
@@ -30,6 +30,11 @@ function drawShape(matrix, offset) {
     });
 }
 
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -42,8 +47,7 @@ function update(time = 0) {
     dropCounter += deltaTime;
 
     if (dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
+        playerDrop();
     }
     
     draw();
@@ -54,5 +58,15 @@ const player = {
     pos: {x: 5, y: 5},
     matrix: matrix,
 }
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39) {
+        player.pos.x++;
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+});
 
 update();
